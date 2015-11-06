af5a0a6dc5813b7bf34647336e81bde48c10d498
Ryan Cotsakis <ryancotsakis@gmail.com>
2015-11-05 18:12:02 -0800

    rabbits eat grass

--- src/ca/ubc/ece/cpen221/mp4/ai/RabbitAI.java
+++ src/ca/ubc/ece/cpen221/mp4/ai/RabbitAI.java
@@ -44,31 +44,86 @@ public class RabbitAI extends AbstractAI {
 		visibleItems = world.searchSurroundings(animal);
 		myX = animal.getLocation().getX();
 		myY = animal.getLocation().getY();
+		foxFound = false;
 		
 		if(numOf("Fox") > 0){
 			foxFound = true;
 			closestFox = getClosest("Fox",animal);
-			int distance = closestFox.getLocation().getDistance(animal.getLocation());
-			if(distance <= DANGER_DISTANCE){
-				int foxX = closestFox.getLocation().getX();
-				int foxY = closestFox.getLocation().getY();
-				if(Math.abs(myX - foxX) > Math.abs(myY - foxY)){
-					if(foxX > myX)
-						dangerDirection = Direction.EAST;
-					else
-						dangerDirection = Direction.WEST;
-				}
-				else{
-					if(foxY > myY)
-						dangerDirection = Direction.NORTH;
-					else
-						dangerDirection = Direction.SOUTH;
-				}
-				Location newLocation = new Location(animal.getLocation(), this.oppositeDir(dangerDirection));
-				if(this.isLocationEmpty(world, animal, newLocation)){
-					return new MoveCommand(animal, newLocation);
+			int foxX = closestFox.getLocation().getX();
+			int foxY = closestFox.getLocation().getY();
+			if(Math.abs(myX - foxX) > Math.abs(myY - foxY)){
+				if(foxX > myX)
+					dangerDirection = Direction.EAST;
+				else
+					dangerDirection = Direction.WEST;
+			}
+			else{
+				if(foxY > myY)
+					dangerDirection = Direction.NORTH;
+				else
+					dangerDirection = Direction.SOUTH;
+			}
+			Location newLocation = new Location(animal.getLocation(), this.oppositeDir(dangerDirection));
+			if(this.isLocationEmpty(world, animal, newLocation)){
+				return new MoveCommand(animal, newLocation);
+			}
+		}
+		
+		if(animal.getEnergy() >= animal.getMinimumBreedingEnergy()){
+			Location breedLocation = new Location(animal.getLocation(), Direction.NORTH);
+			if(this.isLocationEmpty(world, animal, breedLocation)){
+				if(!dangerDirection.equals(Direction.NORTH)){;
+					return new BreedCommand(animal, breedLocation);
 				}
 			}
+			breedLocation = new Location(animal.getLocation(), Direction.EAST);
+			if(this.isLocationEmpty(world, animal, breedLocation)){
+				if(!dangerDirection.equals(Direction.EAST)){;
+					return new BreedCommand(animal, breedLocation);
+				}
+			}
+			breedLocation = new Location(animal.getLocation(), Direction.SOUTH);
+			if(this.isLocationEmpty(world, animal, breedLocation)){
+				if(!dangerDirection.equals(Direction.SOUTH)){
+					return new BreedCommand(animal, breedLocation);
+				}
+			}
+			breedLocation = new Location(animal.getLocation(), Direction.WEST);
+			if(this.isLocationEmpty(world, animal, breedLocation)){
+				if(!dangerDirection.equals(Direction.WEST)){
+					return new BreedCommand(animal, breedLocation);
+				}
+			}
+		}
+		
+		if(numOf("Grass")>0){
+			Item closestGrass = getClosest("Grass", animal);
+			if(closestGrass.getLocation().getDistance(animal.getLocation()) == 1){
+				return new EatCommand(animal, closestGrass);
+			}
+			
+			int grassX = closestGrass.getLocation().getX();
+			int grassY = closestGrass.getLocation().getY();
+			Direction grassDirection;
+			if(Math.abs(myX - grassX) > Math.abs(myY - grassY)){
+				if(grassX > myX)
+					grassDirection = Direction.EAST;
+				else
+					grassDirection = Direction.WEST;
+			}
+			else{
+				if(grassY > myY)
+					grassDirection = Direction.NORTH;
+				else
+					grassDirection = Direction.SOUTH;
+			}
+			
+			Location grassLocation = new Location(animal.getLocation(), grassDirection);
+			if(this.isLocationEmpty(world, animal, grassLocation)){
+				if(!dangerDirection.equals(grassDirection))
+					return new MoveCommand(animal, grassLocation);
+			}
+			
 		}
 		
 		return new WaitCommand();
