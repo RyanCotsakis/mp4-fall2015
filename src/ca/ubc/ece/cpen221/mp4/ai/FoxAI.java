8847d8ecb789ab1ec5a6f4436494f7322c07461d
Tara <tara.ak.sa@gmail.com>
2015-11-06 17:28:17 -0800

    working on foxAI

--- src/ca/ubc/ece/cpen221/mp4/ai/FoxAI.java
+++ src/ca/ubc/ece/cpen221/mp4/ai/FoxAI.java
@@ -20,17 +20,74 @@ import ca.ubc.ece.cpen221.mp4.items.animals.*;
  * Your Fox AI.
  */
 public class FoxAI extends AbstractAI {
-	private int closest = 2; // max number; greater than fox's view range
-
+	private int closest = 15; // max number; greater than fox's view range
+	private Set<Item> ItemsInRange; //set of item's in fox's view range
+	private Location foxLocation;
+	
 	public FoxAI() {
 
 	}
 
 	@Override
 	public Command getNextAction(ArenaWorld world, ArenaAnimal animal) {
-		// TODO: Change this. Implement your own AI to make decisions regarding
-		// the next action.
+	    foxLocation= animal.getLocation(); // get current location of the fox
+	    ItemsInRange= world.searchSurroundings(animal);
+        Item targetRabbit=null; 
+
+	    if(isItemAround("Rabbit")==true){
+	        targetRabbit=getClosest("Rabbit", animal);   
+	        Location targetLocation=targetRabbit.getLocation();
+	        
+	        return new MoveCommand(animal, targetLocation);
+	    }
+
 		return new WaitCommand();
 	}
+	
+	/**
+	 *  Find the closets itemToFind in fox's view range 
+	 *  Precondition: the item must be in fox's view range
+	 * @param itemToFind
+	 * @param animal the fox
+	 * @return
+	 */
+	private Item getClosest(String itemToFind, ArenaAnimal animal){
+	    int smallestDistance=closest;
+	    Item closestItem=null;
+	    
+	       for(Item item: ItemsInRange){
+	           if(item.getName().equals(itemToFind)){
+	               int itemDistance=item.getLocation().getDistance(foxLocation); //gets distance of this item
+	                                                                             // from my location
+	               if(itemDistance<smallestDistance){
+	                   smallestDistance=itemDistance;
+	                   closestItem=item;
+	                   }   
+	               }
+	           }
+	       
+	        if(closestItem == null){
+	            throw new IllegalArgumentException();
+	        }
+	        
+	        return closestItem;
+	}
+	
+	/**
+	 * Checks to see if an item is in the view range of the fox
+	 * @param item
+	 * @return true if item is in the fox's view range and false otherwise
+	 */
+	private boolean isItemAround(String itemName){
+        for(Item item: ItemsInRange){
+            if(item.getName().equals(itemName)){
+                return true;
+                }
+            }
+        return false;
+	}
+	
+	
+
 
 }
