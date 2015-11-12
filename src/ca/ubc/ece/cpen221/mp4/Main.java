3830e4e2bce263f1e25914bc0a3cae656a45f47d
Ryan Cotsakis <ryancotsakis@gmail.com>
2015-11-11 22:09:23 -0800

    love the Util.getDirectionTowards methodgit add .

--- src/ca/ubc/ece/cpen221/mp4/Main.java
+++ src/ca/ubc/ece/cpen221/mp4/Main.java
@@ -19,155 +19,6 @@ import ca.ubc.ece.cpen221.mp4.vehicles.Motorcycle;
  *
  */
 public class Main {
-
-<<<<<<< HEAD
-	static final int X_DIM = 40;
-	static final int Y_DIM = 40;
-	static final int SPACES_PER_GRASS = 7;
-	static final int INITIAL_GRASS = X_DIM * Y_DIM / SPACES_PER_GRASS;
-	static final int INITIAL_GNATS = INITIAL_GRASS / 4;
-	static final int INITIAL_RABBITS = INITIAL_GRASS / 10;
-	static final int INITIAL_FOXES = INITIAL_GRASS / 20;
-	static final int INITIAL_TIGERS = INITIAL_GRASS / 32;
-	static final int INITIAL_BEARS = INITIAL_GRASS / 40;
-	static final int INITIAL_HYENAS = INITIAL_GRASS / 32;
-	static final int INITIAL_CARS = INITIAL_GRASS / 100;
-	static final int INITIAL_TRUCKS = INITIAL_GRASS / 150;
-	static final int INITIAL_MOTORCYCLES = INITIAL_GRASS / 64;
-	static final int INITIAL_MANS = INITIAL_GRASS / 150;
-	static final int INITIAL_WOMANS = INITIAL_GRASS / 100;
-	static final int INITIAL_HUNTERS = INITIAL_GRASS / 150;
-	static final int INITIAL_CROCODILES = INITIAL_GRASS / 20;
-	static final int INITIAL_GNAWTYS = INITIAL_GRASS / 30;
-	static final int INITIAL_BOXFOXES = 2;
-	static final int INITIAL_LIONS = INITIAL_GRASS / 100;
-	
-
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				new Main().createAndShowWorld();
-			}
-		});
-	}
-
-	public void createAndShowWorld() {
-		World world = new WorldImpl(X_DIM, Y_DIM);
-		initialize(world);
-		new WorldUI(world).show();
-	}
-
-	public void initialize(World world) {
-		addGrass(world);
-		world.addActor(new Gardener());
-		addCatapult(world);
-	//	addGnats(world); I hate these things!
-		addRabbits(world);
-		addFoxes(world);
-	//	addCrocodiles(world);
-	//	addGnawtys(world);
-		addPortaGrass(world);
-		addBoxFoxes(world);
-		addLions(world);
-		addMotorcycles(world);
-	}
-
-	private void addGrass(World world) {
-		for (int i = 0; i < INITIAL_GRASS; i++) {
-			Location loc = Util.getRandomEmptyLocation(world);
-			world.addItem(new Grass(loc));
-		}
-	}
-
-	private void addGnats(World world) {
-		for (int i = 0; i < INITIAL_GNATS; i++) {
-			Location loc = Util.getRandomEmptyLocation(world);
-			Gnat gnat = new Gnat(loc);
-			world.addItem(gnat);
-			world.addActor(gnat);
-		}
-	}
-	
-	private void addCrocodiles(World world) {
-		for (int i = 0; i < INITIAL_CROCODILES; i++) {
-			Location loc = Util.getRandomEmptyLocation(world);
-			Crocodile croc = new Crocodile(loc);
-			world.addItem(croc);
-			world.addActor(croc);
-		}
-	}
-	
-	private void addGnawtys(World world) {
-		for (int i = 0; i < INITIAL_GNAWTYS; i++) {
-			Location loc = Util.getRandomEmptyLocation(world);
-			Gnawty gnawty = new Gnawty(loc);
-			world.addItem(gnawty);
-			world.addActor(gnawty);
-		}
-	}
-
-	private void addFoxes(World world) {
-		FoxAI foxAI = new FoxAI();
-		for (int i = 0; i < INITIAL_FOXES; i++) {
-			Location loc = Util.getRandomEmptyLocation(world);
-			Fox fox = new Fox(foxAI, loc);
-			world.addItem(fox);
-			world.addActor(fox);
-		}
-	}
-
-	private void addRabbits(World world) {
-		RabbitAI rabbitAI = new RabbitAI();
-		for (int i = 0; i < INITIAL_RABBITS; i++) {
-			Location loc = Util.getRandomEmptyLocation(world);
-			Rabbit rabbit = new Rabbit(rabbitAI, loc);
-			world.addItem(rabbit);
-			world.addActor(rabbit);
-		}
-	}
-	
-	private void addCatapult(World world){
-		Location loc = Util.getRandomEmptyLocation(world);
-		Catapult catapult = new Catapult(loc);
-		world.addItem(catapult);
-		world.addActor(catapult);
-	}
-	
-	private void addPortaGrass (World world){
-		Location loc = Util.getRandomEmptyLocation(world);
-		PortaGrass portaGrass = new PortaGrass(loc);
-		world.addItem(portaGrass);
-		world.addActor(portaGrass);
-	}
-	
-	private void addBoxFoxes(World world) {
-		for (int i = 0; i < INITIAL_BOXFOXES; i++) {
-			Location loc = Util.getRandomEmptyLocation(world);
-			BoxFox boxFox = new BoxFox(loc);
-			world.addItem(boxFox);
-			world.addActor(boxFox);
-		}
-	}
-	
-	private void addLions(World world) {
-	        for (int i = 0; i < INITIAL_LIONS; i++) {
-	            Location loc = Util.getRandomEmptyLocation(world);
-	            Lion lion = new Lion(loc);
-	            world.addItem(lion);
-	            world.addActor(lion);
-	        }
-	    }
-	
-	private void addMotorcycles(World world) {
-           for (int i = 0; i < INITIAL_MOTORCYCLES; i++) {
-               Location loc = Util.getRandomEmptyLocation(world);
-               Motorcycle motor = new Motorcycle(loc);
-               world.addItem(motor);
-               world.addActor(motor);
-           }
-       }
-=======
     static final int X_DIM = 40;
     static final int Y_DIM = 40;
     static final int SPACES_PER_GRASS = 7;
@@ -216,7 +67,7 @@ public class Main {
         addPortaGrass(world);
         addBoxFoxes(world);
         addLions(world);
-        addMotorcycles(world);
+     //   addMotorcycles(world);
     }
 
     private void addGrass(World world) {
@@ -305,13 +156,12 @@ public class Main {
         }
     }
 
-    private void addMotorcycles(World world) {
+    /*private void addMotorcycles(World world) {
         for (int i = 0; i < INITIAL_MOTORCYCLES; i++) {
             Location loc = Util.getRandomEmptyLocation(world);
             Motorcycle motor = new Motorcycle(loc);
             world.addItem(motor);
             world.addActor(motor);
         }
-    }
->>>>>>> ab565b15a8411430add22dc8438d114a94653415
+    }*/
 }
--- src/ca/ubc/ece/cpen221/mp4/ai/RabbitAI.java
+++ src/ca/ubc/ece/cpen221/mp4/ai/RabbitAI.java
@@ -1,8 +1,6 @@
 package ca.ubc.ece.cpen221.mp4.ai;
 
-import java.util.Iterator;
 import java.util.Set;
-
 import ca.ubc.ece.cpen221.mp4.ArenaWorld;
 import ca.ubc.ece.cpen221.mp4.Direction;
 import ca.ubc.ece.cpen221.mp4.Location;
@@ -12,154 +10,114 @@ import ca.ubc.ece.cpen221.mp4.commands.BreedCommand;
 import ca.ubc.ece.cpen221.mp4.commands.Command;
 import ca.ubc.ece.cpen221.mp4.commands.EatCommand;
 import ca.ubc.ece.cpen221.mp4.commands.MoveCommand;
-import ca.ubc.ece.cpen221.mp4.commands.WaitCommand;
 import ca.ubc.ece.cpen221.mp4.items.Item;
 import ca.ubc.ece.cpen221.mp4.items.animals.ArenaAnimal;
-import ca.ubc.ece.cpen221.mp4.items.animals.Fox;
-import ca.ubc.ece.cpen221.mp4.items.animals.Rabbit;
 
 /**
  * Your Rabbit AI.
  */
-public class RabbitAI extends AbstractAI {
+public class RabbitAI extends AbstractAI{
 
-	private static final int DANGER_DISTANCE = 2;
-	
 	private int closest = 10; // max number; greater than rabbit's view range.
-	private int temp;
-	
+
 	private boolean foxFound;
 	private Item closestFox;
 	private Direction dangerDirection;
-	
-	private int myX;
-	private int myY;
 	Set<Item> visibleItems;
 
-	public RabbitAI() {
+	public RabbitAI ( ){
 	}
 
 	@Override
-	public Command getNextAction(ArenaWorld world, ArenaAnimal animal) {
-		
+	public Command getNextAction (ArenaWorld world, ArenaAnimal animal){
+
 		visibleItems = world.searchSurroundings(animal);
-		myX = animal.getLocation().getX();
-		myY = animal.getLocation().getY();
 		foxFound = false;
-		
-		if(numOf(animal, world, "Fox") > 0){
+
+		if (numOf(animal, world, "Fox") > 0) {
 			foxFound = true;
-			closestFox = getClosest("Fox",animal); //get the closest fox
-			int foxX = closestFox.getLocation().getX();
-			int foxY = closestFox.getLocation().getY();
-			
-			if(Math.abs(myX - foxX) > Math.abs(myY - foxY)){
-				if(foxX > myX)
-					dangerDirection = Direction.EAST;
-				else
-					dangerDirection = Direction.WEST;
-			}
-			
-			else{
-				if(foxY < myY) //north is decreasing y
-					dangerDirection = Direction.NORTH;
-				else
-					dangerDirection = Direction.SOUTH;
-			}
-			
+			closestFox = getClosest("Fox", animal); // get the closest fox
+			dangerDirection = Util.getDirectionTowards(animal.getLocation(), closestFox.getLocation());
 			Location newLocation = new Location(animal.getLocation(), this.oppositeDir(dangerDirection));
-			if(this.isLocationEmpty(world, animal, newLocation)){
+			if (this.isLocationEmpty(world, animal, newLocation)) {
 				return new MoveCommand(animal, newLocation);
 			}
 		}
-		
-		if(animal.getEnergy() >= animal.getMinimumBreedingEnergy()){
+
+		if (animal.getEnergy() >= animal.getMinimumBreedingEnergy()) {
 			Location breedLocation = new Location(animal.getLocation(), Direction.NORTH);
-			if(this.isLocationEmpty(world, animal, breedLocation)){
-				if(!foxFound || (foxFound && !dangerDirection.equals(Direction.NORTH))){
-					if(numOf(animal, world, "grass") > numOf(animal, world, "Rabbit")*2)
+			if (this.isLocationEmpty(world, animal, breedLocation)) {
+				if (!foxFound || (foxFound && !dangerDirection.equals(Direction.NORTH))) {
+					if (numOf(animal, world, "grass") > numOf(animal, world, "Rabbit") * 2)
 						return new BreedCommand(animal, breedLocation);
 				}
 			}
 			breedLocation = new Location(animal.getLocation(), Direction.EAST);
-			if(this.isLocationEmpty(world, animal, breedLocation)){
-				if(!foxFound || (foxFound && !dangerDirection.equals(Direction.EAST))){
-					if(numOf(animal, world, "grass") > numOf(animal, world, "Rabbit")*2)
+			if (this.isLocationEmpty(world, animal, breedLocation)) {
+				if (!foxFound || (foxFound && !dangerDirection.equals(Direction.EAST))) {
+					if (numOf(animal, world, "grass") > numOf(animal, world, "Rabbit") * 2)
 						return new BreedCommand(animal, breedLocation);
 				}
 			}
 			breedLocation = new Location(animal.getLocation(), Direction.SOUTH);
-			if(this.isLocationEmpty(world, animal, breedLocation)){
-				if(!foxFound || (foxFound && !dangerDirection.equals(Direction.SOUTH))){
-					if(numOf(animal, world, "grass") > numOf(animal, world, "Rabbit")*2)
+			if (this.isLocationEmpty(world, animal, breedLocation)) {
+				if (!foxFound || (foxFound && !dangerDirection.equals(Direction.SOUTH))) {
+					if (numOf(animal, world, "grass") > numOf(animal, world, "Rabbit") * 2)
 						return new BreedCommand(animal, breedLocation);
 				}
 			}
 			breedLocation = new Location(animal.getLocation(), Direction.WEST);
-			if(this.isLocationEmpty(world, animal, breedLocation)){
-				if(!foxFound || (foxFound && !dangerDirection.equals(Direction.WEST))){
-					if(numOf(animal, world, "grass") > numOf(animal, world, "Rabbit")*2)
+			if (this.isLocationEmpty(world, animal, breedLocation)) {
+				if (!foxFound || (foxFound && !dangerDirection.equals(Direction.WEST))) {
+					if (numOf(animal, world, "grass") > numOf(animal, world, "Rabbit") * 2)
 						return new BreedCommand(animal, breedLocation);
 				}
 			}
 		}
-		
-		if(numOf(animal, world, "grass")>0){
+
+		if (numOf(animal, world, "grass") > 0) {
 			Item closestGrass = getClosest("grass", animal);
-			if(closestGrass.getLocation().getDistance(animal.getLocation()) == 1){
-				if(numOf(animal, world, "grass") >= numOf(animal, world, "Rabbit") && animal.getEnergy() <= animal.getMaxEnergy()-10)
+			if (closestGrass.getLocation().getDistance(animal.getLocation()) == 1) {
+				if (numOf(animal, world, "grass") >= numOf(animal, world, "Rabbit")
+						&& animal.getEnergy() <= animal.getMaxEnergy() - 10)
 					return new EatCommand(animal, closestGrass);
-			//	return new WaitCommand();
+				// return new WaitCommand();
 			}
-			
-			int grassX = closestGrass.getLocation().getX();
-			int grassY = closestGrass.getLocation().getY();
-			Direction grassDirection;
-			if(grassX > myX)
-				grassDirection = Direction.EAST;
-			else if(grassX < myX)
-				grassDirection = Direction.WEST;
-			else if(grassY < myY) //north is decreasing y
-				grassDirection = Direction.NORTH;
-			else
-				grassDirection = Direction.SOUTH;
-			
+			Direction grassDirection = Util.getDirectionTowards(animal.getLocation(), closestGrass.getLocation());
 			Location grassLocation = new Location(animal.getLocation(), grassDirection);
-			if(this.isLocationEmpty(world, animal, grassLocation)){
-				if(!foxFound || (foxFound && !dangerDirection.equals(grassDirection)))
+			if (this.isLocationEmpty(world, animal, grassLocation)) {
+				if (!foxFound || (foxFound && !dangerDirection.equals(grassDirection)))
 					return new MoveCommand(animal, grassLocation);
 			}
-			
+
 		}
 
 		return MoveCommand.moveInRandomDirection(animal, (World) world);
 	}
-	
-	
+
 	/**
 	 * finds the closest item of some type to the rabbit.
-	 * @param itemName must be in visible range
+	 * 
+	 * @param itemName
+	 *            must be in visible range
 	 * @return the item with the name "itemName" that is closest to the rabbit
 	 */
-	private Item getClosest(String itemName, ArenaAnimal animal){
+	private Item getClosest (String itemName, ArenaAnimal animal){
 		int smallestDistance = closest;
 		Item closestItem = null;
-		for(Item item : visibleItems){
-			if(item.getName().equals(itemName)){
+		for (Item item : visibleItems){
+			if (item.getName().equals(itemName)) {
 				Location myLocation = animal.getLocation();
 				int distance = item.getLocation().getDistance(myLocation);
-				if(distance < smallestDistance && distance != 0){
+				if (distance < smallestDistance && distance != 0) {
 					smallestDistance = distance;
 					closestItem = item;
 				}
 			}
 		}
-		if(closestItem == null){
+		if (closestItem == null) {
 			throw new IllegalArgumentException();
 		}
 		return closestItem;
 	}
-	
-	
-	
 }
